6ccff9bfe8841cf496b8aebc4aead51e9b0be433
nunessilas <50427513+nunessilas@users.noreply.github.com>
2021-01-28 12:25:57 -0300

    Add files via upload

---
+++ PriDesCap/script.js
@@ -0,0 +1,104 @@
+function funcional(){
+    var explicacao = window.document.getElementById('funcional')
+    var titulo = window.document.getElementById('card_funcional')
+    
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'O treinamento funcional se baseia nos movimentos naturais do ser humano, como pular, correr entre outros. O praticante ganha força, equilíbrio, flexibilidade, condicionamento, resistência e agilidade.'
+
+}
+
+function saidafuncional(){
+    var explicacao = window.document.getElementById('funcional')
+    var titulo = window.document.getElementById('card_funcional')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+
+}
+
+function musculacao(){
+    var explicacao = window.document.getElementById('musculacao')
+    var titulo = window.document.getElementById('card_musculacao')
+
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'Ut porta arcu sit amet purus sodales porta. Suspendisse potenti. Nam at sollicitudin ipsum, sed tincidunt sem.    Vestibulum lacus odio, dapibus at ligula eget, facilisis iaculis ipsum.'
+
+}
+
+function saidamusculacao(){
+    var explicacao = window.document.getElementById('musculacao')
+    var titulo = window.document.getElementById('card_musculacao')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+
+}
+
+function crosstrainning(){
+    var explicacao = window.document.getElementById('crosstrainning')
+    var titulo = window.document.getElementById('card_crosstrainning')
+
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'Ut porta arcu sit amet purus sodales porta. Suspendisse potenti. Nam at sollicitudin ipsum, sed tincidunt sem.    Vestibulum lacus odio, dapibus at ligula eget, facilisis iaculis ipsum.'
+
+}
+
+function saidacrosstrainning(){
+    var explicacao = window.document.getElementById('crosstrainning')
+    var titulo = window.document.getElementById('card_crosstrainning')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+}
+
+function pilates(){
+    var explicacao = window.document.getElementById('pilates')
+    var titulo = window.document.getElementById('card_pilates')
+    
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'Ut porta arcu sit amet purus sodales porta. Suspendisse potenti. Nam at sollicitudin ipsum, sed tincidunt sem.    Vestibulum lacus odio, dapibus at ligula eget, facilisis iaculis ipsum.'
+}
+
+function saidapilates(){
+    var explicacao = window.document.getElementById('pilates')
+    var titulo = window.document.getElementById('card_pilates')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+
+}
+
+function dance(){
+    var explicacao = window.document.getElementById('dance')
+    var titulo = window.document.getElementById('card_dance')
+
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'Ut porta arcu sit amet purus sodales porta. Suspendisse potenti. Nam at sollicitudin ipsum, sed tincidunt sem.    Vestibulum lacus odio, dapibus at ligula eget, facilisis iaculis ipsum.'
+}
+
+function saidadance(){
+    var explicacao = window.document.getElementById('dance')
+    var titulo = window.document.getElementById('card_dance')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+
+}
+
+function bike(){
+    var explicacao = window.document.getElementById('bike')
+    var titulo = window.document.getElementById('card_bike')
+
+    titulo.style.marginTop = '20px'
+    explicacao.innerHTML = 'Ut porta arcu sit amet purus sodales porta. Suspendisse potenti. Nam at sollicitudin ipsum, sed tincidunt sem.    Vestibulum lacus odio, dapibus at ligula eget, facilisis iaculis ipsum.'
+
+}
+
+function saidabike(){
+    var explicacao = window.document.getElementById('bike')
+    var titulo = window.document.getElementById('card_bike')
+
+    explicacao.innerHTML = '+SAIBA MAIS'
+    titulo.style.marginTop = '60px'
+
+}
